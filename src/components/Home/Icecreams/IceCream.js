14af3cb0ba58c9f1b0592be7ab9c5646d55cc437
헹구 <hyemin085@gmail.com>
2021-11-03 03:04:41 +0900

    [추가] 2021.11.03 페이지네이션 뷰잡는중

--- src/components/Home/Icecreams/IceCream.js
+++ src/components/Home/Icecreams/IceCream.js
@@ -6,8 +6,211 @@ const IceCream = () => {
   return(
     <>
       <div className="ice-cream">
-        <div className="ice-title">
-          <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FYU2I3%2FbtrjJk7kWzQ%2FBDwUpVMBpKPUoCq5txKQO0%2Fimg.png"/>
+
+        <div className="IceCreams">
+          <ul className="grid-iceCream">
+            <li className="ice">
+              <div className="ice-name">치즈나무 숲</div>
+            <div className="ice-hash"><a>#크림치즈</a> <a>#그린티</a></div>
+            <button className="buy">구매</button>
+              <div className="ice-picture">
+                <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcFs6Bt%2FbtrjHUOQcXK%2FMSXu4TCRYd7Z34f2kK5dT1%2Fimg.png"/>
+              </div>
+              <div className="ice-cone"></div>
+
+            </li>
+            <li className="ice">
+              <div className="ice-name">치즈나무 숲</div>
+              <div className="ice-hash"><a>#크림치즈</a> <a>#그린티</a></div>
+              <button className="buy">구매</button>
+              <div className="ice-picture">
+                <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcFs6Bt%2FbtrjHUOQcXK%2FMSXu4TCRYd7Z34f2kK5dT1%2Fimg.png"/>
+              </div>
+              <div className="ice-cone"></div>
+
+            </li>
+            <li className="ice">
+              <div className="ice-name">치즈나무 숲</div>
+              <div className="ice-hash"><a>#크림치즈</a> <a>#그린티</a></div>
+              <button className="buy">구매</button>
+              <div className="ice-picture">
+                <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcFs6Bt%2FbtrjHUOQcXK%2FMSXu4TCRYd7Z34f2kK5dT1%2Fimg.png"/>
+              </div>
+              <div className="ice-cone"></div>
+
+            </li>
+            <li className="ice">
+              <div className="ice-name">치즈나무 숲</div>
+              <div className="ice-hash"><a>#크림치즈</a> <a>#그린티</a></div>
+              <button className="buy">구매</button>
+              <div className="ice-picture">
+                <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcFs6Bt%2FbtrjHUOQcXK%2FMSXu4TCRYd7Z34f2kK5dT1%2Fimg.png"/>
+              </div>
+              <div className="ice-cone"></div>
+
+            </li>
+            <li className="ice">
+              <div className="ice-name">치즈나무 숲</div>
+              <div className="ice-hash"><a>#크림치즈</a> <a>#그린티</a></div>
+              <button className="buy">구매</button>
+              <div className="ice-picture">
+                <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcFs6Bt%2FbtrjHUOQcXK%2FMSXu4TCRYd7Z34f2kK5dT1%2Fimg.png"/>
+              </div>
+              <div className="ice-cone"></div>
+
+            </li>
+            <li className="ice">
+              <div className="ice-name">치즈나무 숲</div>
+              <div className="ice-hash"><a>#크림치즈</a> <a>#그린티</a></div>
+              <button className="buy">구매</button>
+              <div className="ice-picture">
+                <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcFs6Bt%2FbtrjHUOQcXK%2FMSXu4TCRYd7Z34f2kK5dT1%2Fimg.png"/>
+              </div>
+              <div className="ice-cone"></div>
+
+            </li>
+            <li className="ice">
+              <div className="ice-name">치즈나무 숲</div>
+              <div className="ice-hash"><a>#크림치즈</a> <a>#그린티</a></div>
+              <button className="buy">구매</button>
+              <div className="ice-picture">
+                <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcFs6Bt%2FbtrjHUOQcXK%2FMSXu4TCRYd7Z34f2kK5dT1%2Fimg.png"/>
+              </div>
+              <div className="ice-cone"></div>
+
+            </li>
+            <li className="ice">
+              <div className="ice-name">치즈나무 숲</div>
+              <div className="ice-hash"><a>#크림치즈</a> <a>#그린티</a></div>
+              <button className="buy">구매</button>
+              <div className="ice-picture">
+                <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcFs6Bt%2FbtrjHUOQcXK%2FMSXu4TCRYd7Z34f2kK5dT1%2Fimg.png"/>
+              </div>
+              <div className="ice-cone"></div>
+
+            </li>
+            <li className="ice">
+              <div className="ice-name">치즈나무 숲</div>
+              <div className="ice-hash"><a>#크림치즈</a> <a>#그린티</a></div>
+              <button className="buy">구매</button>
+              <div className="ice-picture">
+                <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcFs6Bt%2FbtrjHUOQcXK%2FMSXu4TCRYd7Z34f2kK5dT1%2Fimg.png"/>
+              </div>
+              <div className="ice-cone"></div>
+
+            </li>
+            <li className="ice">
+              <div className="ice-name">치즈나무 숲</div>
+              <div className="ice-hash"><a>#크림치즈</a> <a>#그린티</a></div>
+              <button className="buy">구매</button>
+              <div className="ice-picture">
+                <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcFs6Bt%2FbtrjHUOQcXK%2FMSXu4TCRYd7Z34f2kK5dT1%2Fimg.png"/>
+              </div>
+              <div className="ice-cone"></div>
+
+            </li>
+            <li className="ice">
+              <div className="ice-name">치즈나무 숲</div>
+              <div className="ice-hash"><a>#크림치즈</a> <a>#그린티</a></div>
+              <button className="buy">구매</button>
+              <div className="ice-picture">
+                <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcFs6Bt%2FbtrjHUOQcXK%2FMSXu4TCRYd7Z34f2kK5dT1%2Fimg.png"/>
+              </div>
+              <div className="ice-cone"></div>
+
+            </li>
+            <li className="ice">
+              <div className="ice-name">치즈나무 숲</div>
+              <div className="ice-hash"><a>#크림치즈</a> <a>#그린티</a></div>
+              <button className="buy">구매</button>
+              <div className="ice-picture">
+                <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcFs6Bt%2FbtrjHUOQcXK%2FMSXu4TCRYd7Z34f2kK5dT1%2Fimg.png"/>
+              </div>
+              <div className="ice-cone"></div>
+
+            </li>
+            <li className="ice">
+              <div className="ice-name">치즈나무 숲</div>
+              <div className="ice-hash"><a>#크림치즈</a> <a>#그린티</a></div>
+              <button className="buy">구매</button>
+              <div className="ice-picture">
+                <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcFs6Bt%2FbtrjHUOQcXK%2FMSXu4TCRYd7Z34f2kK5dT1%2Fimg.png"/>
+              </div>
+              <div className="ice-cone"></div>
+
+            </li>
+            <li className="ice">
+              <div className="ice-name">치즈나무 숲</div>
+              <div className="ice-hash"><a>#크림치즈</a> <a>#그린티</a></div>
+              <button className="buy">구매</button>
+              <div className="ice-picture">
+                <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcFs6Bt%2FbtrjHUOQcXK%2FMSXu4TCRYd7Z34f2kK5dT1%2Fimg.png"/>
+              </div>
+              <div className="ice-cone"></div>
+
+            </li>
+            <li className="ice">
+              <div className="ice-name">치즈나무 숲</div>
+              <div className="ice-hash"><a>#크림치즈</a> <a>#그린티</a></div>
+              <button className="buy">구매</button>
+              <div className="ice-picture">
+                <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcFs6Bt%2FbtrjHUOQcXK%2FMSXu4TCRYd7Z34f2kK5dT1%2Fimg.png"/>
+              </div>
+              <div className="ice-cone"></div>
+
+            </li>
+            <li className="ice">
+              <div className="ice-name">치즈나무 숲</div>
+              <div className="ice-hash"><a>#크림치즈</a> <a>#그린티</a></div>
+              <button className="buy">구매</button>
+              <div className="ice-picture">
+                <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcFs6Bt%2FbtrjHUOQcXK%2FMSXu4TCRYd7Z34f2kK5dT1%2Fimg.png"/>
+              </div>
+              <div className="ice-cone"></div>
+
+            </li>
+            <li className="ice">
+              <div className="ice-name">치즈나무 숲</div>
+              <div className="ice-hash"><a>#크림치즈</a> <a>#그린티</a></div>
+              <button className="buy">구매</button>
+              <div className="ice-picture">
+                <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcFs6Bt%2FbtrjHUOQcXK%2FMSXu4TCRYd7Z34f2kK5dT1%2Fimg.png"/>
+              </div>
+              <div className="ice-cone"></div>
+
+            </li>
+            <li className="ice">
+              <div className="ice-name">치즈나무 숲</div>
+              <div className="ice-hash"><a>#크림치즈</a> <a>#그린티</a></div>
+              <button className="buy">구매</button>
+              <div className="ice-picture">
+                <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcFs6Bt%2FbtrjHUOQcXK%2FMSXu4TCRYd7Z34f2kK5dT1%2Fimg.png"/>
+              </div>
+              <div className="ice-cone"></div>
+
+            </li>
+            <li className="ice">
+              <div className="ice-name">치즈나무 숲</div>
+              <div className="ice-hash"><a>#크림치즈</a> <a>#그린티</a></div>
+              <button className="buy">구매</button>
+              <div className="ice-picture">
+                <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcFs6Bt%2FbtrjHUOQcXK%2FMSXu4TCRYd7Z34f2kK5dT1%2Fimg.png"/>
+              </div>
+              <div className="ice-cone"></div>
+
+            </li>
+            <li className="ice">
+              <div className="ice-name">치즈나무 숲</div>
+              <div className="ice-hash"><a>#크림치즈</a> <a>#그린티</a></div>
+              <button className="buy">구매</button>
+              <div className="ice-picture">
+                <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcFs6Bt%2FbtrjHUOQcXK%2FMSXu4TCRYd7Z34f2kK5dT1%2Fimg.png"/>
+              </div>
+              <div className="ice-cone"></div>
+
+            </li>
+
+          </ul>
         </div>
       </div>
     </>
--- src/components/Home/MainIceCream.js
+++ src/components/Home/MainIceCream.js
@@ -2,12 +2,16 @@ import React, {useRef, useState} from "react";
 import "./MainIceCream.scss"
 import BestIceCream from "./BestIceCream/BestIceCream";
 import IceCream from "./Icecreams/IceCream";
+import Pagination from "react-js-pagination";
 
 
 const Home = () => {
+
+  const [page, setPage] = useState(1);
   const scrollRef = useRef(null);
   const [isDrag, setIsDrag] = useState(false);
   const [startX, setStartX] = useState();
+
 
   const onDragStart = (e) => {
     e.preventDefault();
@@ -55,15 +59,19 @@ const Home = () => {
                     ref={scrollRef}>
       <BestIceCream/>
         </div>
-        <IceCream/>
-        <div>
-          ㅇㅇㅇㅇㅇㅇ
-          ㅇ
-          ㅇ
-          ㅇ
-
+        <div className="ice-title">
+          <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FYU2I3%2FbtrjJk7kWzQ%2FBDwUpVMBpKPUoCq5txKQO0%2Fimg.png"/>
         </div>
+        <IceCream/>
       </div>
+      <Pagination
+        activePage={page}
+        itemsCountPerPage={8}
+        totalItemsCount={20}
+        pageRangeDisplayed={8}
+        prevPageText={"‹"} nextPageText={"›"}
+        // onChange={handlePageChange}
+      />
     </>
   )
 }
