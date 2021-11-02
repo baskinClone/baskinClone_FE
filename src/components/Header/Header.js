a0e3186716efc7bab43c8196635e423a100a2a03
헹구 <hyemin085@gmail.com>
2021-11-02 14:01:01 +0900

    [추가] 2021.11.02 기본 틀 작업

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.scss";
+
+const Header = () => {
+
+  return(
+    <>
+      <ul>
+        <li className="login-form">LOGIN</li>
+        <li>join</li>
+      </ul>
+      <div><img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbGsXYq%2FbtrjBw75AIK%2FonhIS6EdYkPsP68Jjj6MO1%2Fimg.png"/></div>
+    </>
+  )
+}
+
+export default Header;
---
+++ src/components/Home/BestIceCream/BestIceCream.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.scss"
+
+const BestIceCream = () => {
+
+  return(
+    <>
+      <div className="iceCream_main"><img src="https://blog.kakaocdn.net/dn/J5Hqm/btrjDfR0NrQ/p2ZCKBkPGx2zyx1Y5yQz80/img.gif"/></div>
+      <div>여기는 홈이다</div>
+    </>
+  )
+}
+
+export default BestIceCream;
